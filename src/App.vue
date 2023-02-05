e2f51941f75d6f584bd8e491cc9d3f3a5fd25bff
ZanderOl <olexandruk@gmail.com>
2023-02-05 22:10:05 +0200

    install dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello, Vue!</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
