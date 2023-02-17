ee7d7787bcb90ac0a48b89df608f98ab255c18c7
ZanderOl <olexandruk@gmail.com>
2023-02-17 17:42:29 +0200

    Update v-catalog-item.vue
    styles update

--- src/components/catalog/v-catalog-item.vue
+++ src/components/catalog/v-catalog-item.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="v-catalog-item">
     <img class="v-catalog-item__image" v-bind:src="product_data.image" alt="" />
-    <p class="v-catalog-item__name">{{ product_data.title }}</p>
-    <p class="v-catalog-item__pricee">Price: {{ product_data.price }} UAH</p>
-    <button class="v-catalog-item__add-to-cart-btn btn" @click="addToCart">
-      Add to cart
-    </button>
+    <div class="v-catalog-item__description">
+      <p class="v-catalog-item__name">{{ product_data.title }}</p>
+      <p class="v-catalog-item__pricee">Price: {{ product_data.price }} UAH</p>
+      <button class="v-catalog-item__add-to-cart-btn btn" @click="addToCart">
+        Add to cart
+      </button>
+    </div>
   </div>
 </template>
 
@@ -41,13 +43,21 @@ export default {
 
 <style lang="scss">
 .v-catalog-item {
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
   flex-basis: 25%;
   box-shadow: 0 0 8px 0 #e0e0e0;
   padding: 16px;
   margin-bottom: 16px;
 
   &__image {
-    width: 100px;
+    height: 100px;
+  }
+  &__description {
+    justify-content: baseline;
   }
 }
 </style>
