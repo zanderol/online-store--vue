4de825ca63ec4ae95097b6caf9b5762f9b3c3a34
ZanderOl <olexandruk@gmail.com>
2023-04-07 12:55:18 +0300

    Create v-main-page.vue

---
+++ src/components/main-page/v-main-page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-main-page"></div>
+</template>
+
+<script>
+export default {
+  name: "v-main-page",
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+};
+</script>
+<style lang="scss"></style>
