c469b1e7bea9c674c62a7f3598ee1cca91f1fe70
ZanderOl <olexandruk@gmail.com>
2023-04-07 12:56:31 +0300

    Create v-header.vue

---
+++ src/components/layouts/v-header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-header"></div>
+</template>
+
+<script>
+export default {
+  name: "v-header",
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+};
+</script>
+<style lang="scss"></style>
